f010c0c6fa80fa60f085c0e616f2d5f0c090f80a
wendellxavier <wendell_xw20@hotmail.com>
2024-06-29 09:39:44 -0300

    "category"

--- contact/models.py
+++ contact/models.py
@@ -1,6 +1,20 @@
 from django.db import models
 from django.utils import timezone
 
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    
+    
+    
+    def __str__(self) -> str:
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+
 class Contact(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=30, blank=True)
@@ -10,6 +24,7 @@ class Contact(models.Model):
     description = models.TextField(blank=True)
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
     
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
