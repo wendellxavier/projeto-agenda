c3a151e46d981318a32aa8e492908bcc852ee117
wendellxavier <wendell_xw20@hotmail.com>
2024-07-03 08:45:07 -0300

    "header"

--- contact/views.py
+++ contact/views.py
@@ -5,14 +5,14 @@ from .models import Contact
 def index(request):
     contacts = Contact.objects.filter(show=True).order_by('-id')[0:10]
     
-    return render(request, 'contact/index.html', {'contacts': contacts})
+    return render(request, 'contact/index.html', {'contacts': contacts, 'site_title': 'Contatos -'})
 
 
 
 def contact(request, contact_id):
     single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
-    
-    return render(request, 'contact/contact.html', {'contact': single_contact})
+    site_title = f'{single_contact.first_name} {single_contact.last_name} -'
+    return render(request, 'contact/contact.html', {'contact': single_contact, 'site_title': site_title})
 
 
 
