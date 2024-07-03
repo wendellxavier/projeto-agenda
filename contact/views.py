326da31acf05660add6850bbf7cdead4b0686696
wendellxavier <wendell_xw20@hotmail.com>
2024-07-03 09:31:46 -0300

    "search"

--- contact/views.py
+++ contact/views.py
@@ -1,5 +1,6 @@
 from django.http import Http404
-from django.shortcuts import render, get_object_or_404
+from django.db.models import Q
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Contact
 
 def index(request):
@@ -15,4 +16,14 @@ def contact(request, contact_id):
     return render(request, 'contact/contact.html', {'contact': single_contact, 'site_title': site_title})
 
 
+def search(request):
+    search_value = request.GET.get('q', '').strip()
+    
+    if search_value == '':
+        return redirect('contact:index')
+    
+    contacts = Contact.objects.filter(show=True).filter(Q(first_name__icontains=search_value) | Q(last_name__icontains=search_value) |
+                                                        Q(phone__contains=search_value) | Q(email__contains=search_value)).order_by('-id')
+    
+    return render(request, 'contact/index.html', {'contacts': contacts, 'site_title': 'Contatos -'})
 
