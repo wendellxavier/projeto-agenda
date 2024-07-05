6308893f0252edca511661a9f4db9118ad873221
wendellxavier <wendell_xw20@hotmail.com>
2024-07-05 09:10:12 -0300

    "create forms"

---
+++ contact/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from django.core.exceptions import ValidationError
+from .models import Contact
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = ('first_name', 'last_name', 'phone',)
+        
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        self.add_error('first_name', ValidationError('erro'))
+        return super().clean()
--- contact/views.py
+++ contact/views.py
@@ -2,18 +2,6 @@ from django.db.models import Q
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Contact
 from django.core.paginator import Paginator
-from django import forms
-from django.core.exceptions import ValidationError
-
-class ContactForm(forms.ModelForm):
-    class Meta:
-        model = Contact
-        fields = ('first_name', 'last_name', 'phone',)
-        
-    def clean(self):
-        cleaned_data = self.cleaned_data
-        self.add_error('first_name', ValidationError('erro'))
-        return super().clean()
 
 def index(request):
     contacts = Contact.objects.filter(show=True).order_by('-id')
